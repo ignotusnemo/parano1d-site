31e4d637b30df22ec45bdba11e9591323a119822
ignotusnemo <dev@parano1d.org>
2026-08-18 08:45:00 +0100

    Point site to v2.1.0

--- app.js
+++ app.js
@@ -38,7 +38,7 @@
   let repositoryGateAddedDownloadsInert = false;
 
   const launchNoticeStorageKey = "parano1d-testnet-live-2026-08-13-dismissed";
-  const downloadsReleaseTag = "v2.0.1";
+  const downloadsReleaseTag = "v2.1.0";
   try {
     if (localStorage.getItem(launchNoticeStorageKey) === "1") {
       launchNotice.hidden = true;
